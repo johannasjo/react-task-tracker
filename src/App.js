5ce6af06c81deb57a5abcfc1b8cd2b3905401c40
Johanna Sjöstrand <johanna.sjoestrand@gmail.com>
2021-04-06 08:56:14 +0200

    baseline

--- src/App.js
+++ src/App.js
@@ -1,25 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const name = "";
+  return <div className="container">
+    <h1>Hello from react to {name}</h1>
+    <h2>Hello {name ? name : "there is no name"}</h2>
+  </div>;
 }
 
 export default App;
