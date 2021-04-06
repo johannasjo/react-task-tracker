f576361b19b06fc4c49fcfba0c462a5fd705f216
Johanna Sjöstrand <johanna.sjoestrand@gmail.com>
2021-04-06 16:37:16 +0200

    add task to server

--- src/App.js
+++ src/App.js
@@ -26,10 +26,22 @@ function App() {
 
 
   // add task
-  const addTask = (task)=> {
-    const id = Math.floor(Math.random() * 10000) + 1;
-    const newTask = {id, ...task}
-    setTasks([...tasks, task]);
+  const addTask = async (task)=> {
+    const response = await fetch('http://localhost:5000/tasks', {
+      method: 'POST',
+      headers: {
+        'Content-type': 'application/json'
+      }, 
+      body: JSON.stringify(task)
+    })
+
+    const data = await response.json()
+
+    setTasks([...tasks, data])
+
+    // const id = Math.floor(Math.random() * 10000) + 1;
+    // const newTask = {id, ...task}
+    // setTasks([...tasks, task]);
   }
   //delete task
   const deleteTask = async (id) => {
