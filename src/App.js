dfb9c838995f3fb8f2ee403f1cef37a484517681
Johanna Sjöstrand <johanna.sjoestrand@gmail.com>
2021-04-06 11:50:40 +0200

    delete task and toggle status functionality
    it is possible to delete a task using an icon imported from FA,
    also possible to toggle the reminder so that it changes color depending
    on status

--- src/App.js
+++ src/App.js
@@ -1,10 +1,42 @@
 import './App.css';
+import Header from './components/Header';
+import Tasks from './components/Tasks';
+import { useState } from 'react';
 
 function App() {
-  const name = "";
+    const [tasks, setTasks] = useState([
+    {
+      id: 1,
+      text: 'Feed cat',
+      day: 'Jan 4th at 14.30',
+      reminder: true,
+    },
+    {
+      id: 2,
+      text: 'Feed dog',
+      day: 'Jan 5th at 14.30',
+      reminder: true,
+    },
+    {
+      id: 3,
+      text: 'Feed me',
+      day: 'Jan 7th at 00.30',
+      reminder: true,
+    },
+  ]);
+
+  const deleteTask = (id) => {
+    setTasks(tasks.filter((task) => task.id !== id))
+  }
+
+  // Toggle reminder
+  const toggleReminder = (id) => {
+    setTasks(tasks.map((task) => task.id === id ? {...task, reminder: !task.reminder} : task))
+  }
+
   return <div className="container">
-    <h1>Hello from react to {name}</h1>
-    <h2>Hello {name ? name : "there is no name"}</h2>
+     <Header/>
+     {tasks.length > 0 ? <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder}/> : "No tasks to show"}
   </div>;
 }
 
